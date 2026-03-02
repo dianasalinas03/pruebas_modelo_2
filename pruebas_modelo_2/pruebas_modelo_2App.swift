013aa52d60c1cfb124a21f83c8948ea6dcfe68b9
Diana Salinas <diana_salinas03@hotmail.com>
2026-03-02 07:14:19 -0700

    Initial Commit

---
+++ pruebas_modelo_2/pruebas_modelo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  pruebas_modelo_2App.swift
+//  pruebas_modelo_2
+//
+//  Created by Diana⭐ on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct pruebas_modelo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
